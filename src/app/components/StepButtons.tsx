1734c2c4abeb21fbef59e44639ecf9c8868e2f4c
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-10-30 09:21:01 +0500

    Added

--- src/app/components/StepButtons.tsx
+++ src/app/components/StepButtons.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const StepButtons: React.FC = () => {
   return (
-    <div className="max-w-2xl mx-auto flex items-center justify-between px-6">
+    <div className="max-w-2xl mx-auto flex items-center justify-between mt-5">
       <button
         type="submit"
-        className="w-16 h-16 bg-btn-gr hover:bg-btn-gr-reverse flex items-center justify-center text-white rounded-full transition-all duration-200 space-x-2.5 py-5 mt-5"
+        className="w-12 h-12 lg:w-16 lg:h-16 shrink-0 bg-btn-gr hover:bg-btn-gr-reverse flex items-center justify-center text-white rounded-full transition-all duration-200 space-x-2.5 py-5 mt-5"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -22,13 +22,14 @@ const StepButtons: React.FC = () => {
           />
         </svg>
       </button>
-      <p className="text-xl font-medium">
-        Step <span className="font-bold">1</span> of{' '}
-        <span className="font-bold">4</span> to claim reward
+      <p className="md:text-lg lg:text-xl font-medium mt-5">
+        Step <span className="font-bold font-montserrat">1</span> of{' '}
+        <span className="font-bold font-montserrat">4</span> to claim{' '}
+        <span className="hidden sm:inline-block">reward</span>
       </p>
       <button
         type="submit"
-        className="w-16 h-16 bg-btn-gr hover:bg-btn-gr-reverse flex items-center justify-center text-white rounded-full transition-all duration-200 space-x-2.5 py-5 mt-5"
+        className="w-12 h-12 lg:w-16 lg:h-16 shrink-0 bg-btn-gr hover:bg-btn-gr-reverse flex items-center justify-center text-white rounded-full transition-all duration-200 space-x-2.5 py-5 mt-5"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
